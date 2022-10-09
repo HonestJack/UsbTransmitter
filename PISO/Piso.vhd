804f96043688874fbbd01bec844d56227245a140
HonestJack <mucareinicke@gmail.com>
2022-10-09 20:36:37 -0300

    CHANGED: PISO v1 stop when bitstuffing is happening

--- PISO/Piso.vhd
+++ PISO/Piso.vhd
@@ -8,6 +8,7 @@ entity Piso is
          reset : in STD_LOGIC;
 			enable : in STD_LOGIC;
          load : in STD_LOGIC;
+			stuffing : in STD_LOGIC;
          dataIn : in STD_LOGIC_VECTOR(7 downto 0);
 			loadShift: out STD_LOGIC;
          dataOut : out STD_LOGIC
@@ -19,22 +20,24 @@ architecture piso_arc of Piso is
 	SIGNAL counter : Integer range 0 to 255;
 begin
 
-    piso : process (clk, reset, enable, load, dataIn) is
+    piso : process (clk, reset, enable, load, stuffing, dataIn) is
     variable temp : std_logic_vector (7 downto 0);
 	 begin
 		if reset = '1' then
 			temp := (others=>'0');
 			counter <= 0;			
 		elsif clk'event AND clk = '1' AND enable = '1' then
-			if (counter=8) then
-				counter <= 0;
-				loadShift <= '1';
-            temp := dataIn ;
-			else
-				counter <= counter + 1;
-				loadShift <= '0';
-            dataOut <= temp(7);
-            temp := temp(6 downto 0) & '0';
+			if stuffing = '0' then
+				if (counter=8) then
+					counter <= 0;
+					loadShift <= '1';
+					temp := dataIn ;
+				else
+					counter <= counter + 1;
+					loadShift <= '0';
+					dataOut <= temp(7);
+					temp := temp(6 downto 0) & '0';
+				end if;
 			end if;
 		end if;
 	end process piso;
